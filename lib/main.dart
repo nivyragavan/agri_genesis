7b12a727b5589b93daecea469f044f6446e331eb
nivyragavan <nivyragavan@gmail.com>
2024-10-23 20:00:08 +0100

    Status bar added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:agri_genesis/constant/color.dart';
 import 'package:agri_genesis/view/authentication/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 
@@ -14,6 +15,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: primaryColor, // Set status bar color
+    ));
     return GetMaterialApp(
       title: 'AgriGenesis',
       debugShowCheckedModeBanner: false,
